2393ff3a38e89a8ffe47983b5e82bee0b93f9048
daniblank <dan_blank@hotmail.com>
2021-05-13 16:17:36 +0200

    Update Debug

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "4"
+  #define BUILD_NUMBER "17"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.1.210103_4 - 2021-01-03 11:20:19.571947"
+  #define VERSION "v0.1.210513_15 - 2021-05-13 16:16:48.010443"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.1.210103_4"
+  #define VERSION_SHORT "v0.1.210513_15"
 #endif
